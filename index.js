5e032e48023bb9a429a3c6e2d86a52f6d22f426d
Justin <ju3tin@hotmail.co.uk>
2022-08-28 09:32:41 +0100

    sdf

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 app.use(morgan('dev'));
